f08685b66688251b966d1a6f1552725e5b32671a
sabinabernardes <66752747+sabinabernardes@users.noreply.github.com>
2021-01-07 18:52:37 -0300

    Testando a primeira classe de dominio

--- src/main/java/com/example/binon_curso/resources/CategoryResource.java
+++ src/main/java/com/example/binon_curso/resources/CategoryResource.java
@@ -1,16 +1,27 @@
 package com.example.binon_curso.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.binon_curso.domian.Category;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 	@RequestMapping(method = RequestMethod.GET)
 
-	public String listar() {
-		return " REST esta funcionando ";
+	public List<Category> listar() {
+		
+		Category cat1 = new Category(1,"Informatica");
+		Category cat2 = new Category(1,"Escritorio");	
+		List<Category> lista =  new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 }
 
